a6ab557bc77564ef4ab9ee95df1bb3807162322d
malyniak <malyniakvolodiaaa@gmail.com>
2023-06-15 16:42:37 +0300

    add method ciphering text

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.*;
 import java.nio.charset.Charset;
@@ -9,25 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cipher {
-    Path sourcePath;
-    Path outFile;
-    BufferedReader bufferedReader;
-    Key key;
+    private Path sourcePath;
+    private final String SYMBOLS = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ.,\":-? ";
+    private Path outPath;
+    private Key key;
+    private ArrayList<String> originalText;
 
-    public Cipher (Path sourcePath, Key key) {
-        this.sourcePath=sourcePath;
-        this.key=key;
+
+    public Cipher(Path sourcePath, Key key) {
+        this.sourcePath = sourcePath;
+        this.key = key;
     }
-    public ArrayList<String> textFromFile (Path path) {
-        ArrayList<String> text=new ArrayList<>();
-        try(BufferedReader bufferedReader=Files.newBufferedReader(path);
-            BufferedWriter bufferedWriter=Files.newBufferedWriter(path, Charset.defaultCharset())) {
 
-
+    public ArrayList<String> readTextFromFile() {
+        ArrayList<String> originalText = new ArrayList<>();
+        try (BufferedReader bufferedReader = Files.newBufferedReader(sourcePath)) {
+            while (bufferedReader.ready()) {
+                originalText.add(bufferedReader.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Файл" + sourcePath.toString() + " не знайдено. Попробуйте ще раз.");
+        }
+        return originalText;
     }
-        catch (IOException e) {
-                System.out.println("Файл"+path.toString()+" не знайдено. Попробуйте ще раз.");
+
+    public ArrayList<String> cipheringText() {
+        ArrayList<String> changedList = new ArrayList<>();
+        for (String s : originalText) {
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] charsOfLine = s.toCharArray();
+            for (int i = 0; i < charsOfLine.length; i++) {
+                if (SYMBOLS.contains(s.substring(i, i + 1))) {
+                    stringBuilder.append(s.substring(i, i + 1));
+                }
+            }
+            changedList.add(stringBuilder.toString());
         }
-        return text;
-        }
+        return changedList;
+    }
+
 }
