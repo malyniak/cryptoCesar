0558777e9edc0e76702729de3157c5b522ebd2df
malyniak <malyniakvolodiaaa@gmail.com>
2023-06-15 15:16:28 +0300

    add scanner in main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("Виберіть режим роботи програми.\nДля вибору режиму шифрування/дешифрування введіть цифру %1$d\n" +
+                          "Для вибору режиму \"brute force\" введіть цифру %2$d", 1, 2);
+        int  optionOfWork;
+        try(Scanner scanner=new Scanner(System.in)) {
+            optionOfWork=scanner.nextInt();
+            if(optionOfWork==1) {
+
+            }
+
+
+        }
 
     }
+
 }
